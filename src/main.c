a9ffb561dd5e7d9a1e1fe1d7d4ca9136360f4d19
Martha Cuenca Castillo <mcuenca-@car14s5.42barcelona.com>
2025-11-10 19:57:51 +0100

    [refractol]: creawted 3 structs
    t_shr_data for share data. t_mod for share modificable data. t_no_mod for share no modificable data.
    
    norminete: created id[], hashi[] and philos[] with dynamic mem, because flexible array arew forbidden.
    
    leaks: No leaks. A lot data races.

--- src/main.c
+++ src/main.c
@@ -6,14 +6,41 @@
 /*   By: mcuenca- <mcuenca-@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/01 16:29:57 by mcuenca-          #+#    #+#             */
-/*   Updated: 2025/11/10 09:19:30 by mcuenca-         ###   ########.fr       */
+/*   Updated: 2025/11/10 19:35:25 by mcuenca-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 #include <stdio.h>
+#include <sys/time.h>
 
-t_bool	are_int(char **argv, t_data *data)
+unsigned long long	ft_conversion(unsigned long long src, int factor, char op)
+{
+	unsigned long long	target;
+
+	target = 0;
+	if (op == '*')
+		target = src * factor;
+	else if (op == '/')
+		target = src / factor;
+	return (target);
+}
+
+t_bool	*create_hashi(t_no_mod *st)
+{
+	int		i;
+	t_bool *tmp;
+
+	tmp = malloc(st->people * sizeof(int));
+	if (!tmp)
+		return (NULL);
+	i = 0;
+	while (i < st->people)
+		tmp[i++] = TRUE;
+	return (tmp);
+}
+
+t_bool	are_int(char **argv, t_no_mod *st)
 {
 	int	j;
 
@@ -23,23 +50,38 @@ t_bool	are_int(char **argv, t_data *data)
 		if (!ft_str_isdigit(argv[j++]))
 			return (FALSE);
 	}
-	data->people = ft_atoi(argv[1]);
-	data->death = ft_atoi(argv[2]);
-	data->eat = ft_atoi(argv[3]);
-	data->rest = ft_atoi(argv[4]);
+	st->people = ft_atoi(argv[1]);
+	st->death = ft_atoi(argv[2]);
+	st->eat = ft_atoi(argv[3]);
+	st->rest = ft_atoi(argv[4]);
 	if (argv[5])
-		data->times = ft_atoi(argv[5]);
+		st->times = ft_atoi(argv[5]);
 	return (TRUE);
 }
 
+/*pillo el timepo al inicio
+//pillo el tiempo cuando empiece a comer
+//covierto de um a ml
+//resto el tiempo*/
 int	main(int argc, char **argv)
 {
-	t_data				data;
+	t_shr_data		data;
+	t_no_mod		st;
+	t_mod			dy;
+	struct timeval	us;
 
 	if (argc < 5 || argc > 6)
 		return (1);
-	if (!are_int(argv, &data))
+	if (!are_int(argv, &st))
 		return (1);
-	be_philosopher(data.people, &data, routine_mng);
+	if (gettimeofday(&us, NULL))
+		return (1);
+	st.start = ft_conversion(us.tv_usec, 1000, '/');
+	dy.hashi = create_hashi(&st);
+	if (!dy.hashi)
+		return (1);
+	data.st = st;
+	data.dy = dy;
+	be_philosopher(data.st.people, &data, routine_mng);
 	return (0);
 }
