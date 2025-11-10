3e23b84c8433432e051da8b41f2846e3ad54010a
Martha Cuenca Castillo <mcuenca-@car14s5.42barcelona.com>
2025-11-10 10:01:02 +0100

    [feat] [routine]: grab forkS
    Philosophers perform the routine once: start thinking; try to pick up 2 forks, if successful, they eat, otherwise, they keep thiking; and then sleep.
    
    Current fork-taking flow: usleep(10) -> check their own fork(id) and next one (id + 1) -> TRUE/FALSE in share.hashi[id] and single.owned[R/L] -> to eat, then realease forks
    
    NO issues with 10 philosophers or fewr, but data races occur beyond that.

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mcuenca- <mcuenca-@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/01 16:29:57 by mcuenca-          #+#    #+#             */
-/*   Updated: 2025/11/08 14:09:01 by mcuenca-         ###   ########.fr       */
+/*   Updated: 2025/11/10 09:19:30 by mcuenca-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ t_bool	are_int(char **argv, t_data *data)
 	data->people = ft_atoi(argv[1]);
 	data->death = ft_atoi(argv[2]);
 	data->eat = ft_atoi(argv[3]);
-	data->sleep = ft_atoi(argv[4]);
+	data->rest = ft_atoi(argv[4]);
 	if (argv[5])
 		data->times = ft_atoi(argv[5]);
 	return (TRUE);
@@ -35,12 +35,11 @@ t_bool	are_int(char **argv, t_data *data)
 int	main(int argc, char **argv)
 {
 	t_data				data;
-	
-	(void)argv;
+
 	if (argc < 5 || argc > 6)
 		return (1);
 	if (!are_int(argv, &data))
 		return (1);
-	be_philosopher(&data, routine_mng);
+	be_philosopher(data.people, &data, routine_mng);
 	return (0);
 }
--- src/routine.c
+++ src/routine.c
@@ -6,34 +6,91 @@
 /*   By: mcuenca- <mcuenca-@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/05 19:59:34 by mcuenca-          #+#    #+#             */
-/*   Updated: 2025/11/08 14:35:40 by mcuenca-         ###   ########.fr       */
+/*   Updated: 2025/11/10 09:59:07 by mcuenca-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+#include <unistd.h>
 #include <stdio.h>
 #include <pthread.h>
 
+void	think(void)
+{
+	usleep(20);
+}
+
+t_bool	pair_of_hashi(t_id *single, t_data *share)
+{
+	int				id[3];
+
+	pthread_mutex_lock(&share->mutex);
+	id[CURR] = single->id;
+	id[NEXT] = single->id + 1;
+	if (id[NEXT] > share->people - 1)
+		id[NEXT] = 0;
+	if (share->hashi[id[CURR]] == TRUE)
+	{
+		share->hashi[id[CURR]] = FALSE;
+		single->owned[0] = TRUE;
+	}
+	if (share->hashi[id[NEXT]] == TRUE)
+	{
+		share->hashi[id[NEXT]] = FALSE;
+		single->owned[1] = TRUE;
+	}
+	pthread_mutex_unlock(&share->mutex);
+	if (single->owned[0] == TRUE && single->owned[1] == TRUE)
+		return (printf("timestamp_in_ms %i has taken a fork\n", single->id), TRUE);
+	return (FALSE);
+}
+
+void	eat(t_id *single, t_data *share)
+{
+	int	t;
+	int	id[3];
+
+	id[CURR] = single->id;
+	id[NEXT] = single->id + 1;
+	if (id[NEXT] > share->people)
+		id[NEXT] = 0;
+	t = 0;
+	while (t < share->eat)
+		t++;
+	pthread_mutex_lock(&share->mutex);
+	share->hashi[id[CURR]] = TRUE;
+	single->owned[0] = FALSE;
+	pthread_mutex_unlock(&share->mutex);
+	pthread_mutex_lock(&share->mutex);
+	share->hashi[id[NEXT]] = TRUE;
+	single->owned[1] = FALSE;
+	pthread_mutex_unlock(&share->mutex);
+}
+
+void	dream(t_id *single, t_data *share)
+{
+	single->timer = 0;
+	while (single->timer < share->rest)
+	{
+		/* Do not forget to implement starvation time
+		 * And while philo is thiking can die.*/
+		single->timer++;
+	}
+}
+
 void	*routine_mng(void *data)
 {
-	int				i;
-	t_data			*timer;
-	pthread_mutex_t mutex;
+	t_id			*single;
+	t_data			*share;
 
-	
-	i = 1;
-	timer = (t_data *)data;
-	pthread_mutex_init(&mutex, NULL);
-	while (timer->shared_data <= 1000)
-	{
-		pthread_mutex_lock(&mutex);
-		printf("%i\n", timer->shared_data++);
-		pthread_mutex_unlock(&mutex);
-	
-	}
-	//think();
-	//eat();
-	//dream();
-	pthread_mutex_destroy(&mutex);
+	single = (t_id *)data;
+	share = ((t_id *)data)->share;
+	while (pair_of_hashi(single, share) == FALSE)
+		think();/*During x time I check after some time*/
+				/*I have to check if philo is alive*/
+	eat(single, share);
+	/*I have to check if philo is alive*/
+	dream(single, share);
+	/*I have to check if philo is alive*/
 	return (NULL);
 }
